8721a72aaad1b1964e03399fa5fc0c9736faf34d
devcarlosperez <carlosperezsantana04@gmail.com>
2025-10-26 19:41:28 +0000

    Installed sequelize and mysql, and initial database connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require("express")
+const sequelize = require("./db");
 
 const app = express()
 
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true}))
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo")
+})
+
+sequelize.authenticate()
+  .then(() => console.log("Successful mysql connection"))
+  .catch(err => console.error("Error", err));
 
 const port = process.env.port || 8080
 app.listen(port, () => {
