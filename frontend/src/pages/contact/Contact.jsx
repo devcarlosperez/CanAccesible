e53973221656a05bcc680b70515f44e81ec04b6f
devcarlosperez <carlosperezsantana04@gmail.com>
2025-11-10 15:40:20 +0000

    feat: Add Contact page and update routing in App component

---
+++ frontend/src/pages/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+
+const Contact = () => {
+  return (
+    <>
+      <Header transparent={false}/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Contact
