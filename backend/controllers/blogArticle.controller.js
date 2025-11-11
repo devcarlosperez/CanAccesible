bc4135e59b0c20daef360548b5af940db845cb22
devcarlosperez <carlosperezsantana04@gmail.com>
2025-11-11 15:33:10 +0000

    feat: Implement drivers for the BlogArticle model

---
+++ backend/controllers/blogArticle.controller.js
@@ -0,0 +1,22 @@
+const db = require("../models")
+const BlogArticle = db.blogArticle
+
+exports.create = async (req, res) => {
+
+}
+
+exports.findAll = async (req, res) => {
+  
+}
+
+exports.findOne = async (req, res) => {
+
+}
+
+exports.update = async (req, res) => {
+  
+}
+
+exports.delete = async (req, res) => {
+  
+}
