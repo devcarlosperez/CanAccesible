13e1f79667359b965ff014cf3316cde0f1c675f8
devcarlosperez <carlosperezsantana04@gmail.com>
2025-11-12 16:15:04 +0000

    feat: Implement service to manage blog articles

--- frontend/src/pages/blog/Blog.jsx
+++ frontend/src/pages/blog/Blog.jsx
@@ -4,10 +4,29 @@ import Header from "../../components/header/Header";
 const Blog = () => {
   return (
     <>
-      <Header transparent={false}/>
-      <Footer/>
-    </>
-  )
-}
+      <Header transparent={false} />
 
-export default Blog
+      {/* Main Content */}
+      <main className="min-h-screen py-12 mt-15 md:mt-20 lg:mt-25 px-6 bg-gray-200">
+        {/* Title Section */}
+        <div className="text-center mb-12">
+          <h1
+            className="text-3xl md:text-4xl font-poppins font-bold mb-4"
+            style={{ color: "var(--color-neutral-2)" }}
+          >
+            Blog
+          </h1>
+          <p
+            className="text-base font-roboto"
+            style={{ color: "var(--color-neutral-3)" }}
+          >
+            Consulta nuestras noticias más relevantes
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Blog;
