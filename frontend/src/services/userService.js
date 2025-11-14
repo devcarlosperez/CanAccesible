a90afb72a25689f45cdd2ad1806438363151b8d9
jonathanelruso <jonathanmoreraapaza@gmail.com>
2025-11-14 18:19:48 +0000

    feat: Update UserMenu component to display user avatar and improve button styling

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,6 +2,8 @@ import api from "./api";
 
 export const getAllUsers = () => api.get("/users").then((res) => res.data);
 
+export const getUserById = (id) => api.get(`/users/${id}`).then((res) => data);
+
 export const createUser = (userData) =>
   api.post("/users", userData).then((res) => res.data);
 
