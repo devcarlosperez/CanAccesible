49216582de3a2accc58084baf98cbfd19edea6f3
devcarlosperez <carlosperezsantana04@gmail.com>
2025-11-17 17:20:41 +0000

    feat: Update conversation seeder to include additional userId entries

--- backend/seeders/20251026195011-seed-conversations.js
+++ backend/seeders/20251026195011-seed-conversations.js
@@ -1,23 +1,32 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Seeder} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Conversations', [
-      {
-        userId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        userId: 2,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ], {});
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Conversations",
+      [
+        {
+          userId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Conversations', null, {});
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Conversations", null, {});
+  },
 };
