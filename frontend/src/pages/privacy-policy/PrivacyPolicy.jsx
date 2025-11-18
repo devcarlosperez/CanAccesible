ee09b02270af76c85e5f46bbfb41293d87265a59
devcarlosperez <carlosperezsantana04@gmail.com>
2025-11-18 18:47:20 +0000

    feat: Create privacy policy page

---
+++ frontend/src/pages/privacy-policy/PrivacyPolicy.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <Header transparent={false}/>
+      <Footer/>
+    </>
+  )
+}
+
+export default PrivacyPolicy;
