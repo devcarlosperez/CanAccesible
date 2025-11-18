6375a427319c5a5a7f3487e7a211258c2cbfa6b4
devcarlosperez <carlosperezsantana04@gmail.com>
2025-11-18 16:59:52 +0000

    feat: Update seed data for conversation messages with additional entries and corrected sender IDs

--- backend/seeders/20251026195012-seed-conversation-messages.js
+++ backend/seeders/20251026195012-seed-conversation-messages.js
@@ -14,12 +14,36 @@ module.exports = {
       },
       {
         conversationId: 1,
-        senderId: 3,
+        senderId: 2,
         message: 'Bien, gracias. ¿Y tú?',
         dateMessage: new Date(),
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+      {
+        conversationId: 1,
+        senderId: 1,
+        message: 'Tengo una duda, como puedo cambiar mi foto de perfil?',
+        dateMessage: new Date(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        conversationId: 2,
+        senderId: 3,
+        message: 'Buenas, el municipio de Tenerife tiene una duda de como reportar una incidencia.',
+        dateMessage: new Date(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        conversationId: 3,
+        senderId: 3,
+        message: 'Buenos días, tengo una duda respecto a los artículos del blog.',
+        dateMessage: new Date(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
     ], {});
   },
 
