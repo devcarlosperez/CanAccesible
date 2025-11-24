8969a64ab4aa5940482a242fdff71fb8454cf918
jonathanelruso <jonathanmoreraapaza@gmail.com>
2025-11-24 15:19:29 +0000

    fix: Correct typo in SMTP password environment variable

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
   secure: true, // upgrade later with STARTTLS
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.STMP_PASS,
+    pass: process.env.SMTP_PASS,
   },
 });
 
