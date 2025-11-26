6c5a3bdb44dda27565b58a66a4a839c4141c02ab
jonathanelruso <jonathanmoreraapaza@gmail.com>
2025-11-26 19:43:15 +0000

    feat: Implement user management page with filtering and pagination functionality feat: Enhance auth middleware to support token or session verification fix: Update dashboard to include user management link and improve layout

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,25 +1,34 @@
 module.exports = (app) => {
   const user = require("../controllers/user.controller");
   const userImageUpload = require("../middlewares/userImageUpload");
-  const { verifyToken } = require("../middlewares/auth.middleware");
+  const { verifyTokenOrSession } = require("../middlewares/auth.middleware");
 
   const router = require("express").Router();
 
   router.post("/", userImageUpload.single("image"), user.create);
 
-  router.get("/", verifyToken, user.findAll);
+  router.get("/", verifyTokenOrSession, user.findAll);
 
-  router.get("/user", verifyToken, user.findAllUsers);
+  router.get("/user", verifyTokenOrSession, user.findAllUsers);
 
-  router.get("/admin", verifyToken, user.findAllAdmins);
+  router.get("/admin", verifyTokenOrSession, user.findAllAdmins);
 
-  router.get("/top-reporting", verifyToken, user.findTopReportingUsers);
+  router.get(
+    "/top-reporting",
+    verifyTokenOrSession,
+    user.findTopReportingUsers
+  );
 
-  router.get("/:id", verifyToken, user.findOne);
+  router.get("/:id", verifyTokenOrSession, user.findOne);
 
-  router.put("/:id", verifyToken, userImageUpload.single("image"), user.update);
+  router.put(
+    "/:id",
+    verifyTokenOrSession,
+    userImageUpload.single("image"),
+    user.update
+  );
 
-  router.delete("/:id", verifyToken, user.delete);
+  router.delete("/:id", verifyTokenOrSession, user.delete);
 
   app.use("/api/users", router);
 };
